beab9a4750eefeedc84e152c1bb1e7d930c13ac6
koude <koudeauto@gmail.com>
2026-09-19 14:02:53 +0800

    Release Aurora 2026.09.19.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2.11.34"
-            versionCode = 211034
+            versionName = "2026.09.19.1"
+            versionCode = 26091901
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
