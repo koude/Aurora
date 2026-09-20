64350e0055d3bed0d1d38cae19e6df3f3fac119b
koude <koudeauto@gmail.com>
2026-09-20 10:42:00 +0800

    Refine home dashboard hierarchy

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2026.09.19.9"
-            versionCode = 26091909
+            versionName = "2026.09.20.1"
+            versionCode = 26092001
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
